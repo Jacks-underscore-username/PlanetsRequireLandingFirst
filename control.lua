50e1ba83e461a84eefca918ca7c5a89082095410
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2026-05-31 13:57:53 -0500

    fix: fixed a bug where looking at a surface counted as visiting it. fix: fixed a bug where the game would crash when trying to research tech for non planets. locale: now tech names pull from the local correctly.

--- control.lua
+++ control.lua
@@ -1,5 +1,9 @@
 script.on_event(defines.events.on_player_changed_surface, function(event)
     local player = game.get_player(event.player_index)
-    local surface = player.surface.name
-    player.force.script_trigger_research("visit-" .. surface)
+    if (player == nil) then return end
+    local surface = player.character.surface.name
+    local tech_name = "visit-" .. surface
+    if (player.force.technologies[tech_name] ~= nil) then
+        player.force.script_trigger_research(tech_name)
+    end
 end)
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,47 +1,51 @@
 for _, tech in pairs(data.raw["technology"]) do
     ---@type data.SpaceLocationPrototype
     local planet = nil
+    local planet_count = 0
     if (tech.effects ~= nil) then
         for _, effect in pairs(tech.effects) do
             if (effect.space_location ~= nil) then
                 for _, location in pairs(data.raw["planet"]) do
                     if (location.name == effect.space_location) then
                         planet = location
-                        break
+                        planet_count = planet_count + 1
                     end
-                end
-
-                if (planet ~= nil) then
-                    break
                 end
             end
         end
     end
-    if (planet ~= nil) then
+    if (planet ~= nil and planet_count == 1) then
         local tech_name = "visit-" .. planet.name
+        local has_children = false
         for _, sub_tech in pairs(data.raw["technology"]) do
             if (sub_tech.research_trigger ~= nil and sub_tech.prerequisites ~= nil) then
                 for index, parent_name in pairs(sub_tech.prerequisites) do
                     if (parent_name == tech.name) then
+                        has_children = true
                         sub_tech.prerequisites[index] = tech_name
                     end
                 end
             end
         end
+        if (not has_children) then break end
+        local localised_name = { "technology-description.visit-planet", { "space-location-name." .. planet.name } }
         data:extend({
             {
                 research_trigger = {
                     type = "scripted",
-                    trigger_description = "Visit " .. planet.name,
+                    trigger_description = localised_name,
                     icon = "__core__/graphics/icons/entity/character.png"
 
                 },
                 type = tech.type,
                 name = tech_name,
-                localised_name = "Visit " .. planet.name,
+                localised_name = localised_name,
                 prerequisites = { tech.name },
                 icons = {
-                    { icon = planet.icon },
+                    {
+                        icon = planet.icon,
+                        scale = planet.icon_size
+                    },
                     {
                         icon = "__core__/graphics/icons/entity/character.png",
                         scale = 1.5,
