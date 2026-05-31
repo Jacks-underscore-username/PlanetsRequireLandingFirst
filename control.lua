2fd25f1345173f64a557559906b0b98d4f3eaf21
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2026-05-31 17:36:43 -0500

    fix: fixed a crash when in creative mode

--- control.lua
+++ control.lua
@@ -1,6 +1,6 @@
 script.on_event(defines.events.on_player_changed_surface, function(event)
     local player = game.get_player(event.player_index)
-    if (player == nil) then return end
+    if (player == nil or player.character == nil) then return end
     local surface = player.character.surface.name
     local tech_name = "visit-" .. surface
     if (player.force.technologies[tech_name] ~= nil) then
