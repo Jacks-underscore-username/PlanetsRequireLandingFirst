c14be5c7e1e75fec794b7eb6f98e78bf750fe2fd
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2026-06-06 22:01:11 -0500

    fix: fixed a major bug that broke most of the added techs feat: now works with tech that unlocks multiple planets

--- control.lua
+++ control.lua
@@ -1,9 +1,49 @@
+---@type table<string, string[]>
+local planet_map = prototypes.mod_data["planets-require-landing-first-mod-data"].data
+
 script.on_event(defines.events.on_player_changed_surface, function(event)
     local player = game.get_player(event.player_index)
     if (player == nil or player.character == nil) then return end
     local surface = player.character.surface.name
-    local tech_name = "visit-" .. surface
-    if (player.force.technologies[tech_name] ~= nil) then
-        player.force.script_trigger_research(tech_name)
+    log("Player surface: " .. surface)
+    for tech_name, planets in pairs(planet_map) do
+        log("tech_name: " .. tech_name)
+        for _, planet in pairs(planets) do
+            log(" * planet: " .. planet)
+            if (surface == planet) then
+                player.force.script_trigger_research(tech_name)
+            end
+        end
     end
 end)
+
+
+function initializeTech()
+    for _, tech in pairs(game.forces["player"].technologies) do
+        local passed = false
+        for trigger, _ in pairs(planet_map) do
+            if (trigger == tech.name) then
+                passed = true
+                break
+            end
+        end
+
+        if (passed) then
+            passed = false
+            for _, child in pairs(tech.successors) do
+                if (child.researched) then
+                    passed = true
+                    break
+                end
+            end
+        end
+
+        if (passed) then
+            log("Children of " .. tech.name .. " are researched, so it will be too")
+            tech.researched = true
+        end
+    end
+end
+
+script.on_load(initializeTech)
+script.on_init(initializeTech)
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,58 +1,112 @@
+---@type table<string, string[]>
+local planet_map = {}
 for _, tech in pairs(data.raw["technology"]) do
-    ---@type data.SpaceLocationPrototype
-    local planet = nil
-    local planet_count = 0
+    ---@type data.SpaceLocationPrototype[]
+    local planets = {}
     if (tech.effects ~= nil) then
         for _, effect in pairs(tech.effects) do
             if (effect.space_location ~= nil) then
+                if (effect.space_location == "maraxsis-trench" or effect.space_location == "linox-planet_linox" or effect.space_location == "foliax") then goto continue end
                 for _, location in pairs(data.raw["planet"]) do
                     if (location.name == effect.space_location) then
-                        planet = location
-                        planet_count = planet_count + 1
+                        table.insert(planets, location)
                     end
                 end
+                ::continue::
             end
         end
     end
-    if (planet ~= nil and planet_count == 1) then
-        local tech_name = "visit-" .. planet.name
+    if (#planets > 0) then
+        local id = ""
+        for index, planet in pairs(planets) do
+            if (index == 1) then
+                id = id .. planet.name
+            else
+                id = id .. "-" .. planet.name
+            end
+        end
+        log("Making tech for planet " .. id)
+        local tech_name = "visit-" .. id
+        planet_map[tech_name] = {}
+        for _, planet in pairs(planets) do
+            table.insert(planet_map[tech_name], planet.name)
+        end
         local has_children = false
         for _, sub_tech in pairs(data.raw["technology"]) do
             if (sub_tech.research_trigger ~= nil and sub_tech.prerequisites ~= nil) then
                 for index, parent_name in pairs(sub_tech.prerequisites) do
                     if (parent_name == tech.name) then
                         has_children = true
+                        log(" * Moving child tech " .. sub_tech.name)
                         sub_tech.prerequisites[index] = tech_name
                     end
                 end
             end
         end
-        if (not has_children) then break end
-        local localised_name = { "technology-description.visit-planet", { "space-location-name." .. planet.name } }
-        data:extend({
+        local localised_name
+        if (#planets == 1) then
+            localised_name = { "technology-description.visit-planet", { "space-location-name." .. planets[1].name } }
+        else
+            localised_name = { "technology-description.visit-planet-or", { "space-location-name." .. planets[1].name } }
+            local prev = localised_name
+            for index, planet in pairs(planets) do
+                if (index > 1) then
+                    if (index == #planets) then
+                        local tmp = { "technology-description.visit-planet-or-chain", { "space-location-name." .. planet.name } }
+                        table.insert(prev, tmp)
+                        prev = tmp
+                    else
+                        table.insert(prev, { "space-location-name." .. planet.name })
+                    end
+                end
+            end
+        end
+        ---@type (data.IconData)[]
+        local icons
+        if (tech.icons == nil) then
+            icons = { {
+                icon = tech.icon,
+                icon_size = tech.icon_size
+            } }
+        else
+            ---@diagnostic disable-next-line: cast-local-type
+            icons = table.deepcopy(tech.icons)
+        end
+        ---@diagnostic disable-next-line: param-type-mismatch
+        table.insert(icons,
             {
-                research_trigger = {
-                    type = "scripted",
-                    trigger_description = localised_name,
-                    icon = "__core__/graphics/icons/entity/character.png"
-
-                },
-                type = tech.type,
-                name = tech_name,
-                localised_name = localised_name,
-                prerequisites = { tech.name },
-                icons = {
-                    {
-                        icon = planet.icon,
-                        scale = planet.icon_size
-                    },
-                    {
-                        icon = "__core__/graphics/icons/entity/character.png",
-                        scale = 1.5,
-                        shift = { 32, 32 }
-                    }
-                }
+                icon = "__core__/graphics/icons/entity/character.png",
+                scale = 1.5,
+                shift = { 32, 32 }
             }
-        })
+        )
+        if (has_children) then
+            data:extend({
+                {
+                    research_trigger = {
+                        type = "scripted",
+                        trigger_description = localised_name,
+                        icon = "__core__/graphics/icons/entity/character.png"
+
+                    },
+                    type = tech.type,
+                    name = tech_name,
+                    localised_name = localised_name,
+                    prerequisites = { tech.name },
+                    icons = icons
+                }
+            })
+        else
+            log(" * Skipped " .. tech_name .. " as it has no children")
+        end
     end
 end
+
+
+data:extend({
+    {
+        type = "mod-data",
+        name = "planets-require-landing-first-mod-data",
+        data = planet_map
+    }
+})
